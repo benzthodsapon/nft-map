68d71c63f08ac3fa60c8112d935459ca2baf257a
Thodsapon Tantitham <55072426+benzthodsapon@users.noreply.github.com>
2022-07-11 00:50:05 +0700

    90 MAP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 import { GetDataMapService } from './services/get-data-map.service';
-import { VERSION, ViewChild, ElementRef } from "@angular/core";
 import { ThisReceiver } from '@angular/compiler';
 
 @Component({
@@ -9,6 +8,7 @@ import { ThisReceiver } from '@angular/compiler';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'nft-map';
 
   checkAPI = true;
@@ -32,6 +32,14 @@ export class AppComponent {
   @ViewChild('myCanvas', { static: true }) 
   myCanvas: ElementRef<HTMLCanvasElement>;
 
+  public context: CanvasRenderingContext2D | null 
+  viewInitialized = false;
+  zooming = true;
+  yOffset: number;
+  sideLength: number;
+  totalSize;
+
+
   private ctx: CanvasRenderingContext2D | null 
   constructor(
     private getDataMapService : GetDataMapService,
@@ -44,8 +52,21 @@ export class AppComponent {
       this.pacman = this.#image("pacman.png")
       this.ghost = this.#image("yellowDot.png")
       this.pic5 = this.#image("eiei.png")
+      this.yOffset = 20;
+      this.sideLength = 160;
+      this.totalSize = 200;
   }
-  
+
+  @HostListener('wheel', ['$event']) onMouseWheel(event: WheelEvent) {
+    if (this.zooming) {
+      this.sideLength = this.sideLength - event.deltaY;
+      this.drawMap();
+    } else {
+      this.yOffset = this.yOffset - event.deltaY;
+      this.drawMap();
+    }
+  }
+
   ngOnInit(): void {
     this.ctx = (this.myCanvas.nativeElement as HTMLCanvasElement).getContext('2d');
 
@@ -99,6 +120,14 @@ export class AppComponent {
 
   drawMap(){
     this.process();
+    if (!(this.ctx = (this.myCanvas.nativeElement as HTMLCanvasElement).getContext('2d'))) {
+        throw new Error(`2d context not supported or canvas already initialized`);
+   } 
+    this.ctx.save();
+    this.ctx.clearRect(0, 0, this.totalSize, this.totalSize);
+    this.ctx.fillStyle = 'rgb(0,200,20)';
+    this.ctx.fillRect(0, this.yOffset, this.sideLength, this.sideLength);
+    this.ctx.restore();
   }
 
   process(){
@@ -176,6 +205,10 @@ export class AppComponent {
         if(element.location[0] && element.location[1]){
           let indexY = 0;
           let indexX = 0;
+          let tmp = element.location[0];
+          let tmp1 = element.location[1];
+          element.location[0] = -tmp1;
+          element.location[1] = tmp;
           if(element.location[0] > -74){
              indexX = element.location[0]+74
           }
@@ -188,18 +221,29 @@ export class AppComponent {
           if(element.location[1] < -74){
              indexY = element.location[1]-74
           }
+          if(element.location[0] == 0){
+            indexY = element.location[0]
+          }
+          if(element.location[1] == 0){
+            indexY = element.location[1]
+          }
           if(indexX >= 0 && indexY >= 0){
-            this.map[indexX][indexY] = 0;
+            this.map[indexX+23-37][indexY] = 0;
           } 
           if(indexX < 0 && indexY < 0){
             console.log("indexX",element.location[0])
             console.log("indexY",element.location[1])
           }
         }
-      } else if (element.type.height== 3 && element.type.width== 3){
+      } 
+      if (element.type.height== 3 && element.type.width== 3){
         if(element.location[0] && element.location[1]){
           let indexY = 0;
           let indexX = 0;
+          let tmp = element.location[0];
+          let tmp1 = element.location[1];
+          element.location[0] = -tmp1;
+          element.location[1] = tmp;
           if(element.location[0] > -74){
             indexX = element.location[0]+74
          }
@@ -221,15 +265,20 @@ export class AppComponent {
 
           for(let i = 0 ; i < 3 ;i++){
             for(let j = 0 ; j < 3;j++){
-              this.map[indexX+i][indexY+j] = 2;
+              this.map[indexX+i+21-37][indexY+j] = 2;
             }
           }
          } 
         }
-      } else if (element.type.height== 6 && element.type.width== 6){
+      }
+       if (element.type.height== 6 && element.type.width== 6){
         if(element.location[0] && element.location[1]){
           let indexY = 0;
           let indexX = 0;
+          let tmp = element.location[0];
+          let tmp1 = element.location[1];
+          element.location[0] = -tmp1;
+          element.location[1] = tmp;
           if(element.location[0] > -74){
             indexX = element.location[0]+74
          }
@@ -245,15 +294,20 @@ export class AppComponent {
          if(indexX >= 0 && indexY >= 0){
           for(let i = 0 ; i < 6 ;i++){
             for(let j = 0 ; j < 6;j++){
-              this.map[indexX+i][indexY+j] = 3;
+              this.map[indexX+i+18-37][indexY+j] = 3;
             }
           }
          } 
         }
-      } else if (element.type.height== 12 && element.type.width== 12){
+      } 
+       if (element.type.height== 12 && element.type.width== 12){
         if(element.location[0] && element.location[1]){
           let indexY = 0;
           let indexX = 0;
+          let tmp = element.location[0];
+          let tmp1 = element.location[1];
+          element.location[0] = -tmp1;
+          element.location[1] = tmp;
           if(element.location[0] > -74){
             indexX = element.location[0]+74
          }
@@ -269,15 +323,21 @@ export class AppComponent {
          if(indexX >= 0 && indexY >= 0){
           for(let i = 0 ; i < 12 ;i++){
             for(let j = 0 ; j < 12;j++){
-              this.map[indexX+i][indexY+j] = 4;
+              this.map[indexX+i+12-37][indexY+j] = 4;
             }
           }
          } 
         }
-      } else if (element.type.height== 24 && element.type.width== 24){
+      } 
+      else if (element.type.height== 24 && element.type.width== 24){
         if(element.location[0] && element.location[1]){
           let indexY = 0;
           let indexX = 0;
+          let tmp = element.location[0];
+          let tmp1 = element.location[1];
+          element.location[0] = -tmp1;
+          element.location[1] = tmp;
+
           if(element.location[0] > -74){
             indexX = element.location[0]+74
          }
@@ -293,7 +353,7 @@ export class AppComponent {
          if(indexX >= 0 && indexY >= 0){
           for(let i = 0 ; i < 24 ;i++){
             for(let j = 0 ; j < 24;j++){
-              this.map[indexX+i][indexY+j] = 5;
+              this.map[indexX+i-37][indexY+j] = 5;
             }
           }
            
@@ -303,7 +363,11 @@ export class AppComponent {
         
     })
     console.log("Test",this.map)
+    this.drawMap();
 
   }
-  
+
+
+ 
 }
+  
